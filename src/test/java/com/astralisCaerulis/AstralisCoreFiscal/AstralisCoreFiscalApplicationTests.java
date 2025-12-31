172c2ce55165d14d4a365696d54bd35d8a84b5a3
Gustavo <gustavod.oliveira@outlook.com>
2025-12-31 14:06:19 -0300

    Initial commit: Spring Boot project with MySQL configuration

---
+++ src/test/java/com/astralisCaerulis/AstralisCoreFiscal/AstralisCoreFiscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astralisCaerulis.AstralisCoreFiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstralisCoreFiscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
