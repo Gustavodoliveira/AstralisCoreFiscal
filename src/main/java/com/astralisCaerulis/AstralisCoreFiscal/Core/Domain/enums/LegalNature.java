17b33aa9d2c557555751ba2b76e8baeb1f332143
Gustavo <133404360+Gustavodoliveira@users.noreply.github.com>
2025-12-31 13:48:16 -0300

    Delete src directory

--- src/main/java/com/astralisCaerulis/AstralisCoreFiscal/Core/Domain/enums/LegalNature.java
+++
@@ -1,7 +0,0 @@
-package com.astralisCaerulis.AstralisCoreFiscal.Core.Domain.enums;
-
-public enum LegalNature {
-  MEI,
-  LTDA,
-  EIRELI
-}
--- src/main/java/com/astralisCaerulis/AstralisCoreFiscal/Core/Domain/enums/TaxRegime.java
+++
@@ -1,7 +0,0 @@
-package com.astralisCaerulis.AstralisCoreFiscal.Core.Domain.enums;
-
-public enum TaxRegime {
-  Simples_Nacional,
-  Lucro_Presumido,
-  Lucro_Real
-}
